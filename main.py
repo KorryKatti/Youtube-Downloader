dea5302b36109450c831d8e74ed9e8d7c539f4ed
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-05-03 11:05:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox, QFileDialog
 from pytube import YouTube
 
 class YouTubeDownloader(QWidget):
@@ -21,12 +22,24 @@ class YouTubeDownloader(QWidget):
         self.path_label = QLabel("Enter Output Directory Path:", self)
         self.path_label.move(20, 60)
         self.path_entry = QLineEdit(self)
-        self.path_entry.setGeometry(180, 60, 200, 20)
+        self.path_entry.setGeometry(180, 60, 150, 20)
+        self.browse_button = QPushButton("Browse", self)
+        self.browse_button.setGeometry(340, 60, 60, 20)
+        self.browse_button.clicked.connect(self.browse_directory)
 
         # Download button
         self.download_button = QPushButton("Download Video", self)
         self.download_button.setGeometry(150, 100, 120, 30)
         self.download_button.clicked.connect(self.download_video)
+
+        # Set default output path to user's home directory
+        self.default_output_path = os.path.expanduser("~")
+        self.path_entry.setText(self.default_output_path)
+
+    def browse_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory", self.default_output_path)
+        if directory:
+            self.path_entry.setText(directory)
 
     def download_video(self):
         video_url = self.url_entry.text()
